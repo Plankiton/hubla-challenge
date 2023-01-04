e8d70af6ea7a076eacbf4d7bda95ed003f538ee7
Yaks Souza <pl4nk1ton@gmail.com>
2023-01-04 13:42:24 -0300

    chore: move code

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,6 +1,12 @@
 package api
 
 import (
+	"math"
+	"mime/multipart"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/plankiton/hubla-challenge/pkg/db"
 )
@@ -33,4 +39,82 @@ func NewRepositories(pgPool *pgxpool.Pool) *Repositories {
 			pgPool.Close()
 		},
 	}
+}
+
+func toSale(content []byte) *db.Sale {
+	curr := 0
+
+	typ := content[curr]
+	curr++
+
+	date := content[curr : curr+dateSize]
+	curr += dateSize
+
+	prod := string(content[curr : curr+prodSize])
+	curr += prodSize
+
+	value := content[curr : curr+valueSize]
+	curr += valueSize
+
+	saler := string(content[curr:])
+
+	valueInt, _ := strconv.Atoi(string(value))
+	valueParsed := moveFloat(valueInt, -2)
+
+	dateParsed, _ := time.Parse(time.RFC3339, string(date))
+
+	typParsed, _ := strconv.Atoi(string(typ))
+
+	sale := &db.Sale{
+		Type:    typParsed,
+		Date:    dateParsed,
+		Value:   valueParsed,
+		Product: strings.TrimSpace(prod),
+		Saler:   strings.TrimSpace(saler),
+	}
+
+	return sale
+}
+
+func openFile(f multipart.File, size int) ([][]byte, error) {
+	content := make([]byte, size)
+
+	_, err := f.Read(content)
+	lines := [][]byte{}
+
+	c := 0
+	for i := range content {
+		if content[i] == '\n' {
+			if c > i {
+				break
+			}
+
+			lines = append(lines, content[c:i])
+			c = i + 1
+		}
+	}
+
+	return lines, err
+}
+
+func moveFloat(value int, points int) float64 {
+	return float64(value) * math.Pow(10.0, float64(points))
+}
+
+const (
+	saleSize  = 86
+	salerSize = 10
+	valueSize = 10
+	prodSize  = 30
+	dateSize  = 25
+	typSize   = 1
+)
+
+const defaultFilename = "sales"
+
+var saleType = map[string]string{
+	"1": "Venda produtor",
+	"2": "Venda afiliado",
+	"3": "Comissão paga",
+	"4": "Comissão recebida",
 }
